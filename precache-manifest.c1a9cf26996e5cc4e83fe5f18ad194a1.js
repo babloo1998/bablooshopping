a1ba86648d56b44f8026c371bdd159248cb085d3
babloo <babloo.mishra@tothenew.com>
2019-07-17 11:52:02 +0530

    Updates

---
+++ precache-manifest.c1a9cf26996e5cc4e83fe5f18ad194a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78b73d3ca8a4669584555fd31b07537f",
+    "url": "/shopping_cart/index.html"
+  },
+  {
+    "revision": "e73919b1cabec74a00b4",
+    "url": "/shopping_cart/static/css/main.58838daf.chunk.css"
+  },
+  {
+    "revision": "83f44b70f7426cd06b7c",
+    "url": "/shopping_cart/static/js/2.508681e4.chunk.js"
+  },
+  {
+    "revision": "e73919b1cabec74a00b4",
+    "url": "/shopping_cart/static/js/main.4b5fd78f.chunk.js"
+  },
+  {
+    "revision": "0708d0012bbe56ac2cc7",
+    "url": "/shopping_cart/static/js/runtime~main.e41427e9.js"
+  }
+]);
